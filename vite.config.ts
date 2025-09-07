974049289839bfc4dcbed778603d890e67e43ef0
LongVuHoang <longvu09092004@gmail.com>
2025-09-07 14:38:29 +0700

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import fs from "fs";
+const isDev = process.env.NODE_ENV === "development";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,12 +12,18 @@ export default defineConfig({
     },
   },
   server: {
-    https: {
-      key: fs.readFileSync(
-        path.resolve(__dirname, "./certs/localhost+2-key.pem")
-      ),
-      cert: fs.readFileSync(path.resolve(__dirname, "./certs/localhost+2.pem")),
-    },
-    port: 5173,
+    ...(isDev &&
+    fs.existsSync(path.resolve(__dirname, "./certs/localhost+2-key.pem"))
+      ? {
+          https: {
+            key: fs.readFileSync(
+              path.resolve(__dirname, "./certs/localhost+2-key.pem")
+            ),
+            cert: fs.readFileSync(
+              path.resolve(__dirname, "./certs/localhost+2.pem")
+            ),
+          },
+        }
+      : {}),
   },
 });
