bee9d6627b445c477d36455f7b504e69d4268c81
LongVuHoang <longvu09092004@gmail.com>
2025-11-29 14:06:10 +0700

    Fix random img

--- src/utils/imageRandom.ts
+++ src/utils/imageRandom.ts
@@ -7,29 +7,7 @@ import Y1 from "@/assets/Y1.jpg";
 
 const fallbackImages = [E1, M1, M2, S1, Y1];
 
-export function getRandomLocalImage() {
+export async function getRandomImage() {
   const index = Math.floor(Math.random() * fallbackImages.length);
   return fallbackImages[index];
-}
-
-// --- API for random anime image ---
-const API_URL = "https://api.waifu.pics/sfw/waifu";
-
-export async function getRandomImage() {
-  try {
-    const controller = new AbortController();
-    const timeout = setTimeout(() => controller.abort(), 200);
-
-    const res = await fetch(API_URL, { signal: controller.signal });
-    clearTimeout(timeout);
-
-    if (!res.ok) throw new Error("API error");
-    const data = await res.json();
-
-    if (!data?.url) throw new Error("Invalid API format");
-
-    return data.url;
-  } catch (error) {
-    return getRandomLocalImage(); // fallback local
-  }
 }
